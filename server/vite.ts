eaad7409f96e4e97ff4adeef77a95df48ba068c0
wtlz <vitaly1313@tut.by>
2025-11-02 16:05:24 +0100

    fix: downgrade drizzle-kit to version 0.18.1 for compatibility
    refactor: update path resolution in vite.ts and vite.config.ts to use __dirname for better clarity and reliability
    
    chore: adjust static file serving path in serveStatic function to point to the correct distribution directory

--- server/vite.ts
+++ server/vite.ts
@@ -5,6 +5,9 @@ import { createServer as createViteServer, createLogger } from "vite";
 import { type Server } from "http";
 import viteConfig from "../vite.config";
 import { nanoid } from "nanoid";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const viteLogger = createLogger();
 
@@ -45,12 +48,7 @@ export async function setupVite(app: Express, server: Server) {
     const url = req.originalUrl;
 
     try {
-      const clientTemplate = path.resolve(
-        import.meta.dirname,
-        "..",
-        "client",
-        "index.html",
-      );
+      const clientTemplate = path.resolve(__dirname, "..", "client", "index.html");
 
       // always reload the index.html file from disk incase it changes
       let template = await fs.promises.readFile(clientTemplate, "utf-8");
@@ -68,7 +66,8 @@ export async function setupVite(app: Express, server: Server) {
 }
 
 export function serveStatic(app: Express) {
-  const distPath = path.resolve(import.meta.dirname, "public");
+  // папка сборки клиента
+  const distPath = path.resolve(__dirname, "..", "dist", "public");
 
   if (!fs.existsSync(distPath)) {
     throw new Error(
